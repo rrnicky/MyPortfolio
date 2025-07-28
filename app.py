eade9f1414af13dda4d04b8e91014418b2ccb72f
Rashmi <rrcniky@gmail.com>
2025-07-28 23:46:24 +0530

    css changes

--- app.py
+++ app.py
@@ -5,39 +5,46 @@ app = Flask(__name__)
 projects = [
     {
         "title": "Gmail Cleaner",
-        "desc": "AI-powered tool to organize and delete unwanted Gmail emails.",
+        "desc": "AI-powered mobile app to organize and delete unwanted Gmail emails based on existing and AI generated categories such as promotions/social/ecommerce etc and sender information",
         "tech": ["Python", "FastAPI", "OAuth2", "ML"],
         "img": "gmail.png",
         "link": "#"
     },
     {
         "title": "WhatsApp Media Cleaner",
-        "desc": "App to remove duplicate WhatsApp sent images already present in gallery.",
+        "desc": "Mobile App to remove duplicate WhatsApp sent media already present in gallery and unwanted whatsapp media from phone storage",
         "tech": ["Flutter", "Image Hashing", "Dart"],
         "img": "whatsapp.png",
         "link": "#"
     },
     {
-        "title": "GradeEmailer",
-        "desc": "Send personalized grade emails using name and roll_no from CSV.",
-        "tech": ["Flask", "CSV", "Email"],
-        "img": "gradeemailer.png",
+        "title": "LibrarySpaceMax",
+        "desc": "Mobile app for students to reserve seats in library to save time and effort of manually checking for space. Qr code based seat booking and real time space availability in Library with seat layout UI (In Progress)",
+        "tech": ["Flask", "Flutter", "QR Code", "Dart", "OAuth2"],
+        "img": "libApp.png",
         "link": "#"
     },
     {
-        "title": "DataValidationTool",
-        "desc": "Validates data in raw files against predefined rules",
-        "tech": ["Flask", "XML", "MongoDB"],
-        "img": "dataValidation.png",
+        "title": "DataViz",
+        "desc": "Web app to create custom visualizations, preprocess and analyze data, build ML models (In Progress)",
+        "tech": ["Flask", "CSV/Excel","Dash", "Data Visualization", "ML Models", "Data Analysis" ],
+        "img": "dataviz.png",
         "link": "#"
     },
-    {
-        "title": "DocumentWriter",
-        "desc": "Creates documents with relevant extracted information from raw files ",
-        "tech": ["Flask", "XML", "MongoDB", "AWS SQS", "AWS S3"],
-        "img": "documentWriter.png",
-        "link": "#"
-    }
+    # {
+    #     "title": "DataValidationTool"
+    #     "desc": "Validates data in raw files against predefined rules",
+    #     "tech": ["Flask", "XML", "MongoDB"],
+    #     "img": "dataValidation.png",
+    #     "link": "#"
+    # },
+    # {
+    #     "title": "DocumentWriter",
+    #     "desc": "Creates documents with relevant extracted information from raw files ",
+    #     "tech": ["Flask", "XML", "MongoDB", "AWS SQS", "AWS S3"],
+    #     "img": "documentWriter.png",
+    #     "link": "#"
+    # }
 ]
 
 skills = ["Python", "Flask", "FastAPI", "OAuth2", "Machine Learning"]
